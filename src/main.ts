09f70d2613c9376de29fde7d644dc673cfa9d700
Martijn Faber <martijn@fabtronics.nl>
2021-07-28 16:02:53 +0200

    feat(config): add config module and set prefix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/modules/app/app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get<ConfigService>(ConfigService);
+
+  app.setGlobalPrefix(config.get<string>('api.prefix'));
+  await app.listen(config.get<number>('api.port'));
 }
 bootstrap();
--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
+import config from 'config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+    }),
+  ],
 })
 export class AppModule {}
